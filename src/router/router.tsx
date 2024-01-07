6dbd86515389051da47ea5596cfcf770ea3a7df5
Bar Cohen <sscohen99@gmail.com>
2024-01-07 13:05:35 +0200

    configured layout routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router-dom'
 import RootLayout from '@/pages/layouts/RootLayout.tsx'
 import HomePage from '@/pages/HomePage.tsx'
+import ShopLayout from '@/pages/layouts/ShopLayout.tsx'
 
 export const router = createBrowserRouter([
     {
@@ -8,8 +9,25 @@ export const router = createBrowserRouter([
         element: <RootLayout/>,
         children: [
             {
-                index: true,
-                element: <HomePage/>
+                path: '',
+                children: [{
+                    path: 'login',
+                    // element: <LoginPage/>
+                },
+                    {
+                        path: 'signup',
+                        // element: <SignupPage/>
+                    }]
+            },
+            {
+                path: '',
+                element: <ShopLayout/>,
+                children: [
+                    {
+                        index: true,
+                        element: <HomePage/>
+                    }
+                ]
             }
         ]
     }
