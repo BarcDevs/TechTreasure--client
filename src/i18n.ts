161c78311c3f3462e054ffc3e3eaf37d63b92310
Bar Cohen <sscohen99@gmail.com>
2024-01-09 13:11:22 +0200

    fixed i18next setup

---
+++ src/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18next'
+import {initReactI18next} from 'react-i18next'
+
+export default i18n.use(initReactI18next)
+    .init({
+        lng: 'en',
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false
+        },
+        resources: {
+            en: {
+                ...(await import('./translations/en.json')).default
+            }
+        }
+    })
--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/globals.css'
 import './styles/theme.css'
-import i18n from 'i18next'
-import {I18nextProvider} from 'react-i18next'
-
-i18n.init({
-    // lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-        escapeValue: false
-    },
-    resources: {
-        en: {
-            translation: import('./translations/en.json')
-        }
-    }
-})
+import './i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <I18nextProvider i18n={i18n}>
-            <App/>
-        </I18nextProvider>
+        <App/>
     </StrictMode>
 )
