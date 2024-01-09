9cd3e3d51077fe4f5e41b497e4abd521d72fbb30
Bar Cohen <sscohen99@gmail.com>
2024-01-10 00:32:22 +0200

    removed reference comment

--- src/components/home/Sidebar.tsx
+++ src/components/home/Sidebar.tsx
@@ -11,15 +11,15 @@ const SidebarItem = ({category}: { category: CATEGORIES | { name_key: string, su
     const {t} = useTranslation(I18N_NAMESPACES.categories)
     return typeof category === 'string' ? (
         <li key={category} className={'w-full hover:opacity-90'}>
-            <Link to={category} className="text-body text-center text-black">
+            <Link to={category} className="text-body text-start text-black">
                 {t(category)}
             </Link>
         </li>
     ) : (
         <DropdownMenu>
             <li className={'w-full hover:opacity-90'}>
-                <DropdownMenuTrigger className="w-full inline-flex justify-between">
-                    <p className="text-body text-center text-black">
+                <DropdownMenuTrigger className="inline-flex w-full justify-between">
+                    <p className="text-body text-start text-black">
                         {t(category.name_key)}
                     </p>
                     <Icon key={category.name_key} name={'chevron'} path={dropdown}
@@ -42,7 +42,7 @@ const SidebarItem = ({category}: { category: CATEGORIES | { name_key: string, su
 
 const Sidebar = ({}) => (
     <aside>
-        <ul className={'flex-col-start w-fit gap-4 pr-3'}>
+        <ul className={'flex-col-start w-fit gap-4'}>
             {GROUPED_CATEGORIES.map(category =>
                 <SidebarItem
                     key={typeof category === 'string' ? category : category.name_key}
@@ -53,30 +53,3 @@ const Sidebar = ({}) => (
 )
 
 export default Sidebar
-
-/* <div className="w-[217px] h-[344px] flex-col justify-start items-start gap-4 inline-flex">
-<div className="justify-start items-start gap-[51px] inline-flex">
-    <div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Woman’s
-        Fashion
-    </div>
-    <div className="w-6 h-6 relative origin-top-left -rotate-90"/>
-</div>
-<div className="justify-start items-start gap-[81px] inline-flex">
-    <div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Men’s
-        Fashion
-    </div>
-    <div className="w-6 h-6 relative origin-top-left -rotate-90"/>
-</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Electronics</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Home & Lifestyle
-</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Medicine</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Sports & Outdoor
-</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Baby’s & Toys
-</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Groceries & Pets
-</div>
-<div className="text-center text-black text-base font-normal font-['Poppins'] leading-normal">Health & Beauty
-</div>
-</div> */
