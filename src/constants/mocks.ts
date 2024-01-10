def68fc9c1989177b6cfdda448a0e7e5313db153
Bar Cohen <sscohen99@gmail.com>
2024-01-10 04:07:12 +0200

    added mock items

---
+++ src/constants/mocks.ts
@@ -0,0 +1,52 @@
+import {Item} from '@/types'
+
+export const ITEMS: Item[] = [
+    {
+        id: "i109",
+        name: 'HAVIT HV-G92 Gamepad',
+        image: '/assets/images/items/item-1.png',
+        rating: 4.3,
+        votes: 584,
+        discount: 40,
+        price: 120,
+        oldPrice: 160
+    },
+    {
+        id: "i110",
+        name: 'AK-900 Wired Keyboard',
+        image: '/assets/images/items/item-2.png',
+        rating: 4.8,
+        votes: 283,
+        discount: 35,
+        price: 960,
+        oldPrice: 1160
+    },
+    {
+        id: "i111",
+        name: 'IPS LCD Gaming Monitor',
+        image: '/assets/images/items/item-3.png',
+        rating: 4.9,
+        votes: 157,
+        price: 370,
+        discount: 30,
+        oldPrice: 400
+    },
+    {
+        id: "i112",
+        name: 'RGB liquid CPU Cooler',
+        image: '/assets/images/items/item-4.png',
+        rating: 4.1,
+        votes: 97,
+        price: 160,
+        discount: 0,
+        oldPrice: 170
+    },
+    {
+        id: "i113",
+        name: 'ASUS FHD Gaming Laptop',
+        image: '/assets/images/items/item-5.png',
+        rating: 5,
+        votes: 987,
+        price: 700,
+    }
+]
