5aec53809c3072c8cef68edbdae02550f50a2628
Bar Cohen <sscohen99@gmail.com>
2024-01-10 02:25:08 +0200

    added rating component

---
+++ src/components/shared/Rating.tsx
@@ -0,0 +1,17 @@
+import fullStar from '/assets/icons/star-filled.svg'
+import halfStar from '/assets/icons/star-half.svg'
+import emptyStar from '/assets/icons/star-empty.svg'
+import Icon from '@/components/shared/Icon.tsx'
+
+const Rating = ({rating}: { rating: number }) => (
+    <div className={'inline-flex'}>
+        {[...Array(5)].map((_, i) => (
+            rating >= i + .9 ? <Icon key={i} path={fullStar} name="star"/> :
+                rating >= i + .4 ? <Icon key={i} path={halfStar} name="star"/> :
+                    <Icon key={i} path={emptyStar} name="star"/>
+        ))}
+    </div>
+)
+
+
+export default Rating
