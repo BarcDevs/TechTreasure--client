bc770cc6e51300937f504f45e912dc3a9087ca9f
Bar Cohen <sscohen99@gmail.com>
2024-01-12 00:17:13 +0200

    added homepage layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,23 @@
 import Sidebar from '@/components/home/Sidebar.tsx'
 import Hero from '@/components/home/hero.tsx'
+import ItemRow from '@/components/shop/ItemRow.tsx'
+import {ITEMS} from '@/constants/mocks.ts'
 
 const HomePage = ({}) => {
     return (
-        <main className={'my-5 ml-[5vw]'}>
-            <div className={'inline-flex h-fit w-[90vw] justify-start gap-[3.75rem]'}>
+        <main className={'flex-col-start my-5 ml-[5vw] w-[90vw] gap-[8.75rem]'}>
+            <div className={'inline-flex h-fit w-full justify-start gap-[3.75rem]'}>
                 <Sidebar/>
                 <Hero/>
             </div>
+            {/* TODO add translations */}
+            <ItemRow name={'Today\'s'} headline={'Flash Sales'} items={ITEMS} scroll={'horizontal'} timerEnd={new Date('2024-01-20')}/>
+            {/*<ItemList name={'Categories'} headline={'Browse By Category'}/> /!* not an item list, TODO change to category list *!/*/}
+            <ItemRow name={'This Month'} headline={'Best Selling Products'} items={ITEMS.filter((_, i) => i < 4)} scroll={'none'}/>
+            {/* Featured Sale */}
+            <ItemRow name={'Our Products'} headline={'Explore Our Products'} items={ITEMS} rows={2} scroll={'vertical'}/>
+            {/* New Arrivals */}
+            {/* free delivery, customer service 24/7, money-back guarantee */}
         </main>
     )
 }
