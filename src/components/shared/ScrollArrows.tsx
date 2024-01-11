21135489cf3b4c0dd0638e4b8d37363de424fb38
Bar Cohen <sscohen99@gmail.com>
2024-01-11 17:09:43 +0200

    added scroll arrows

---
+++ src/components/shared/ScrollArrows.tsx
@@ -0,0 +1,19 @@
+import {forwardRef, MutableRefObject} from 'react'
+import {CarouselRef} from '@/types/ui'
+import Icon from '@/components/shared/Icon.tsx'
+import arrowRight from '/assets/icons/arrow-right.svg'
+import arrowLeft from '/assets/icons/arrow-left.svg'
+
+const ScrollArrows = forwardRef<CarouselRef>(({}, ref) =>
+    (
+        <div className={'flex-row'}>
+            <button className={'p-2.5'} onClick={()=> (ref as MutableRefObject<CarouselRef>).current?.prev()}>
+                <Icon path={arrowLeft} name={'arrow-left'} size={24}/>
+            </button>
+            <button className={'p-2.5'} onClick={()=> (ref as MutableRefObject<CarouselRef>).current?.next()}>
+                <Icon path={arrowRight} name={'arrow-right'} size={24}/>
+            </button>
+        </div>
+    ))
+
+export default ScrollArrows
