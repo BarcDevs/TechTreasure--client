c4e65fffbff71c65529aa450476527a208d649e6
Bar Cohen <sscohen99@gmail.com>
2024-01-12 05:22:50 +0200

    added some todos for future completion

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,10 +16,10 @@ const HomePage = ({}) => {
             <ItemRow name={'Today\'s'} headline={'Flash Sales'} items={ITEMS} scroll={'horizontal'} timerEnd={new Date('2024-01-20')}/>
             <CategoryRow name={'Categories'} headline={'Browse By Category'} categories={Object.values(Categories)}/>
             <ItemRow name={'This Month'} headline={'Best Selling Products'} items={ITEMS.filter((_, i) => i < 4)} scroll={'none'}/>
-            {/* Featured Sale */}
+            {/* TODO Featured Sale */}
             <ItemRow name={'Our Products'} headline={'Explore Our Products'} items={ITEMS} rows={2} scroll={'vertical'}/>
-            {/* New Arrivals */}
-            {/* free delivery, customer service 24/7, money-back guarantee */}
+            {/* TODO New Arrivals */}
+            {/* TODO free delivery, customer service 24/7, money-back guarantee */}
         </main>
     )
 }
