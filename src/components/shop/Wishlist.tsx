0ec2fe5c5e1b09a58e00573ad0fe21c35a1871f2
Bar Cohen <sscohen99@gmail.com>
2024-01-15 01:52:25 +0200

    added move all to cart button

--- src/components/shop/Wishlist.tsx
+++ src/components/shop/Wishlist.tsx
@@ -1,16 +1,24 @@
 import RowHeader from '@/components/shared/RowHeader.tsx'
 import {ITEMS} from '@/constants/mocks.ts'
 import Item from '@/components/shop/Item.tsx'
+import Button from '@/components/elements/Button.tsx'
 
 const Wishlist = ({}) => {
     const wishlistCount = 5
     const removeFromWishlist = () => {
         console.log('remove from wishlist')
     }
+
+    const addAllToCart = () => {
+
+    }
+
     return (
         <div className={'flex-center-col w-full gap-[60px]'}>
-            <RowHeader headline={`Wishlist (${wishlistCount})`} variant={'white'}/>
-            <ul className={'flex-start-between flex-row flex-wrap gap-5 max-sm:flex-center-col'}>
+            <RowHeader headline={`Wishlist (${wishlistCount})`} variant={'white'}>
+                <Button variant={'white'} text={'Move All To Cart'} onClick={addAllToCart}/>
+            </RowHeader>
+            <ul className={'flex-start-between max-sm:flex-center-col flex-row flex-wrap gap-5'}>
                 {ITEMS.map(item => (
                     <li key={item.id}>
                         <Item item={item} variant={'wishlist'} onDelete={removeFromWishlist}/>
