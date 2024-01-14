a8b94ce8c2d35c56a5decd9f8be01ff9557ebe81
Bar Cohen <sscohen99@gmail.com>
2024-01-14 05:09:48 +0200

    fixed responsiveness of auth page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -2,8 +2,8 @@ import SignupForm from '@/components/auth/SignupForm.tsx'
 
 const AuthPage = ({type}: { type: 'login' | 'signup' }) => (
     <main className={'flex-center-row'}>
-        <div className={'w-[50vw]'}>
-            <img src="/assets/images/auth-side-image.png" alt="side-image" className={'max-sm:hidden'}/>
+        <div className={'max-md:hidden md:w-[50vw]'}>
+            <img src="/assets/images/auth-side-image.png" alt="side-image"/>
         </div>
         <div className={'flex-center w-[50vw]'}>
             {type === 'signup' ? <SignupForm/> : /*<LoginForm/>*/ null}
