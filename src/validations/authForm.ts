4a8ff608d748e826f99eadb168c846f1be8327dd
Bar Cohen <sscohen99@gmail.com>
2024-01-14 03:21:27 +0200

    added messages to form validations

--- src/validations/authForm.ts
+++ src/validations/authForm.ts
@@ -1,14 +1,21 @@
 import * as z from "zod"
 
 const signupFormSchema = z.object({
-    name: z.string().min(2).max(50).regex(/^[a-zA-Z ']+$/),
-    email: z.string().email(),
-    password: z.string().min(8).max(50),
+    name: z.string()
+        .min(2, {message: "Name must be at least 2 characters long"})
+        .max(50)
+        .regex(/^[a-zA-Z ']+$/, {message: "Name can only contain letters, spaces, and apostrophes"}),
+    email: z.string().email({message: "Must be a valid email"}),
+    password: z.string()
+        .min(8, {message: "Password must be at least 8 characters long"})
+        .max(50)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+            {message: "Password must contain at least one uppercase letter, one lowercase letter, and one number"}),
 })
 
 const loginFormSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8).max(50),
+    email: z.string().email({message: "Invalid email"}),
+    password: z.string().min(8, {message: "Invalid password"}).max(50),
 })
 
 export type SignupForm = z.infer<typeof signupFormSchema>
