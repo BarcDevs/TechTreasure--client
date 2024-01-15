65d219f5a2f733de417b479b90c79e31256fb8be
Bar Cohen <sscohen99@gmail.com>
2024-01-15 02:37:30 +0200

    added for you list

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,9 +1,10 @@
 import Wishlist from "@/components/shop/Wishlist"
+import ForYou from '@/components/shop/ForYou.tsx'
 
 const WishlistPage = ({}) => (
-    <main className={'flex_col ml-[5vw] w-[90vw]'}>
+    <main className={'flex_col ml-[5vw] w-[90vw] gap-20'}>
         <Wishlist/>
-        {/*<ForYou/>*/}
+        <ForYou/>
     </main>
 )
 
