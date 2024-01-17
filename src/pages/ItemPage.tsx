8921203794dec2799dc97104af824baff22e6bf4
Bar Cohen <sscohen99@gmail.com>
2024-01-17 05:05:49 +0200

    added item page layout

---
+++ src/pages/ItemPage.tsx
@@ -0,0 +1,9 @@
+const ItemPage = ({}) => (
+    <main className={'flex_col ml-[5vw] w-[90vw] gap-[140px] max-sm:ml-[2.5vw] max-sm:w-[95vw] max-sm:gap-20'}>
+        {/*<ItemDetails/>*/}
+        {/*<RelatedItems/>*/}
+    </main>
+)
+
+
+export default ItemPage
