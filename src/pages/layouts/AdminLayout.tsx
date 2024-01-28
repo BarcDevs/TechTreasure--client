67e05902a5a9a95a332f56e383f796cb0c71972c
Bar Cohen <sscohen99@gmail.com>
2024-01-29 01:17:06 +0200

    added protection to admin root

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -1,20 +1,34 @@
-import {Outlet} from 'react-router-dom'
+import {Outlet, useNavigate} from 'react-router-dom'
 
 import Header from '@/components/admin/layout/Header.tsx'
 import Sidebar from '@/components/admin/layout/Sidebar.tsx'
+import {useSelector} from 'react-redux'
+import {IRootState} from '@/store'
+import {useEffect} from 'react'
 
-const AdminLayout = ({}) => (
-    <>
-        <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-            <Sidebar/>
-            <div className="flex_col">
-                <Header/>
-                <Outlet/>
-                {/*<Footer/>*/}
+const AdminLayout = ({}) => {
+    const user = useSelector((state: IRootState) => state.auth.user)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!user || user.role !== 'seller')
+            navigate('/', {replace: true})
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <>
+            <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+                <Sidebar/>
+                <div className="flex_col">
+                    <Header/>
+                    <Outlet/>
+                    {/*<Footer/>*/}
+                </div>
             </div>
-        </div>
 
-    </>
-)
+        </>
+    )
+}
 
 export default AdminLayout
