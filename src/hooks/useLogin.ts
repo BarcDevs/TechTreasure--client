92ebd53e12c56cad2c1f9499af1cccd3b4caca13
Bar Cohen <sscohen99@gmail.com>
2024-01-29 01:08:18 +0200

    added effect to useLogin to redirect back to root if user is already logged in

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,12 +1,19 @@
 import {MutationFunction, useMutation} from '@tanstack/react-query'
 import {storeToken} from '@/api/auth.ts'
 import {loginAction} from '@/store/authSlice.ts'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
+import {useEffect} from 'react'
 
 export const useLogin = (loginCallback: MutationFunction<any, any>) => {
+    const isLoggedIn = useSelector((state: any) => state.auth.isAuthenticated)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+
+    useEffect(() => {
+        if (isLoggedIn)
+            navigate('/', {replace: true})
+    }, [])
 
     return useMutation({
         mutationFn: loginCallback,
