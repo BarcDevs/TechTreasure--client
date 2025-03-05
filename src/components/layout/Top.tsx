7510cf3ac1d024172b2a140a3b72be1f078e4bc0
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-05 17:55:05 +0200

    feat: add link to promo's 'shop now' button

--- src/components/layout/Top.tsx
+++ src/components/layout/Top.tsx
@@ -12,10 +12,10 @@ const Top = ({}) => {
             <div className={'w-[100px] max-md:hidden'}/>
             <div className="flex items-center justify-center gap-2">
                 <div
-                    className="text-small h-fit w-fit text-neutral-50">
+                    className="text-small size-fit text-neutral-50">
                     {t(GLOBAL_LOCALES.promo)}
                 </div>
-                <Link to={'/'}
+                <Link to={'/products/660dd51e612c62d3b92c2b4d'}
                       className="text-center font-poppins text-sm font-semibold leading-normal text-neutral-50 underline max-md:hidden">
                     {t(GLOBAL_LOCALES.shopNow)}
                 </Link>
