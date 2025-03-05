348aea9bc99ce4a9420fcb4fadab1097924e9fdc
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-05 17:46:47 +0200

    feat: add link and translation to 'view all' button on wishlist

--- src/components/shop/ForYou.tsx
+++ src/components/shop/ForYou.tsx
@@ -2,21 +2,29 @@ import RowHeader from '@/components/shared/RowHeader.tsx'
 import Button from '@/components/elements/Button.tsx'
 import {ITEMS} from '@/constants/mocks.ts'
 import Item from '@/components/shop/Item.tsx'
+import {GLOBAL_LOCALES, I18N_NAMESPACES} from '@/constants/locales.ts'
+import {useTranslation} from 'react-i18next'
+import {Link} from 'react-router-dom'
 
-const ForYou = ({}) => (
-    <section className={'flex-center-col w-full gap-[60px]'}>
-        <RowHeader headline={'Just For You'} variant={'white'}>
-            {/* TODO add functionality */}
-            <Button variant={'white'} text={'View All'}/>
-        </RowHeader>
-        <ul className={'flex-start-around max-sm:flex-center-col flex-row flex-wrap gap-5'}>
-            {[...ITEMS.slice(0, 4)].map(item => (
-                <li key={item._id} className={'shrink-0'}>
-                    <Item item={item}/>
-                </li>
-            ))}
-        </ul>
-    </section>
-)
+const ForYou = ({}) => {
+    const {t} = useTranslation([I18N_NAMESPACES.global])
+
+    return (
+        <section className={'flex-center-col w-full gap-[60px]'}>
+            <RowHeader headline={'Just For You'} variant={'white'}>
+                <Link to={`/products`}>
+                    <Button variant={'white'} text={t(GLOBAL_LOCALES.viewAll)}/>
+                </Link>
+            </RowHeader>
+            <ul className={'flex-start-around max-sm:flex-center-col flex-row flex-wrap gap-5'}>
+                {[...ITEMS.slice(0, 4)].map(item => (
+                    <li key={item._id} className={'shrink-0'}>
+                        <Item item={item}/>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
 
 export default ForYou
