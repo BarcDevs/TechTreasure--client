27d72d61485e53b41a777d0d1a8be742bdc3c52c
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-13 16:39:12 +0200

    fix: search input now cleared if user change page

--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -3,16 +3,18 @@ import search from '/assets/icons/search.svg'
 import {useTranslation} from 'react-i18next'
 import {GLOBAL_LOCALES, I18N_NAMESPACES} from '@/constants/locales.ts'
 import {useLocation, useNavigate, useSearchParams} from 'react-router-dom'
+import {useEffect, useRef} from 'react'
 
 const Search = ({additionalStyles}: { additionalStyles?: string }) => {
     const {t} = useTranslation(I18N_NAMESPACES.global)
     const navigate = useNavigate()
     const location = useLocation()
     const [searchTerm, setSearchParams] = useSearchParams()
+    const searchInputRef = useRef<HTMLInputElement>(null)
 
     const searchString = searchTerm.get('search')
 
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleSearch = (e: React.ChangeEvent<any>) => {
         setSearchParams((prev) => {
             prev.set('search', e.target.value)
             if (!e.target.value)
@@ -27,10 +29,18 @@ const Search = ({additionalStyles}: { additionalStyles?: string }) => {
         }
     }
 
+    useEffect(() => {
+        if (!location.pathname.includes('/products') &&
+            searchInputRef.current) {
+            searchInputRef.current.value = ''
+        }
+    }, [location.pathname])
+
     return (
         <div className={`inline-flex h-6 w-[210px] justify-between border-none ${additionalStyles}`}>
             <input
                 type="text"
+                ref={searchInputRef}
                 value={searchString as string}
                 onChange={handleSearch}
                 onClick={handleSearchClick}
