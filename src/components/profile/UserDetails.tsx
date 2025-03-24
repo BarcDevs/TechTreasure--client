4558914e4617a9b8b4e4f3122b082181941bc138
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-03-24 14:22:57 +0200

    style: reformat code

--- src/components/profile/UserDetails.tsx
+++ src/components/profile/UserDetails.tsx
@@ -5,30 +5,35 @@ type Props = {
     user: BaseUser
 }
 
-const UserDetails: FC<Props> = ({user}) => {
-
-    return (
+const UserDetails: FC<Props> = ({user}) =>
+    (
         <main className="container mx-auto flex-1 px-4 py-8">
             <div className="w-full">
                 <div className="rounded-md border border-gray-200 p-6">
                     <div className="gap-x-8 gap-y-6">
                         <div className="flex flex-col gap-6">
                             <div>
-                                <h3 className="mb-2 text-sm font-medium text-gray-700">Full Name</h3>
+                                <h3 className="mb-2 text-sm font-medium text-gray-700">
+                                    Full Name
+                                </h3>
                                 <div className="rounded-md bg-gray-100 p-3 text-sm text-gray-800">
                                     {user?.name}
                                 </div>
                             </div>
 
                             <div>
-                                <h3 className="mb-2 text-sm font-medium text-gray-700">Email</h3>
+                                <h3 className="mb-2 text-sm font-medium text-gray-700">
+                                    Email
+                                </h3>
                                 <div className="rounded-md bg-gray-100 p-3 text-sm text-gray-800">
                                     {user?.email}
                                 </div>
                             </div>
 
                             <div>
-                                <h3 className="mb-2 text-sm font-medium text-gray-700">Address</h3>
+                                <h3 className="mb-2 text-sm font-medium text-gray-700">
+                                    Address
+                                </h3>
                                 <div className="rounded-md bg-gray-100 p-3 text-sm text-gray-800">
                                     {user?.address || 'N/A'}
                                 </div>
@@ -39,6 +44,5 @@ const UserDetails: FC<Props> = ({user}) => {
             </div>
         </main>
     )
-}
 
 export default UserDetails
