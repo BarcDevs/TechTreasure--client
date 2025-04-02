d7a55a4149bd1df5eeaeac729a4b14791d585620
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-04-02 15:27:17 +0300

    feat: add seller homepage

---
+++ src/pages/seller/SellerHomePage.tsx
@@ -0,0 +1,10 @@
+import StoreDashboard from '@/components/seller/StoreDashboard.tsx'
+
+const SellerHomePage = () =>
+    (
+        <main className="min-h-screen bg-gray-50 p-4 md:p-6">
+            <StoreDashboard/>
+        </main>
+    )
+
+export default SellerHomePage
