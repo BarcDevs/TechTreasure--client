ad8d104b163c39eebebaeba569fc2a10d127bf84
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-04-02 16:34:57 +0300

    feat: made the sidebar badges fix the actual data

--- src/components/seller/layout/SidebarLink.tsx
+++ src/components/seller/layout/SidebarLink.tsx
@@ -1,8 +1,10 @@
 import {SELLER_LINKS, SELLER_ROOT} from '@/constants'
 import {NavLink} from 'react-router-dom'
 import {Badge} from '@/components/ui/badge.tsx'
+import ORDERS from '@/mock/orders.ts'
+import CUSTOMERS from '@/mock/customers.ts'
 
-const SidebarLink = ({link} : {link: typeof SELLER_LINKS[number]}) => (
+const SidebarLink = ({link}: { link: typeof SELLER_LINKS[number] }) => (
     <NavLink
         className={({isActive}) =>
             `flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${isActive ?
@@ -10,11 +12,13 @@ const SidebarLink = ({link} : {link: typeof SELLER_LINKS[number]}) => (
         to={link.href}
         end={link.href === SELLER_ROOT}
     >
-        <link.icon className="h-4 w-4"/>
+        <link.icon className="size-4"/>
         <p>{link.name}</p>
         {(link.name === 'Orders' || link.name === 'Customers') && <Badge
-            className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-red-500 hover:bg-red-500/80">
-            6
+            className="ml-auto flex size-6 shrink-0 items-center justify-center rounded-full bg-red-500 hover:bg-red-500/80">
+            {link.name === 'Orders' ?
+                ORDERS.length :
+                CUSTOMERS.length}
         </Badge>}
     </NavLink>
 )
