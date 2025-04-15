79fd6602806b3781ee2a6fa933273d988a3db01a
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-04-15 19:53:26 +0300

    rfc: remove sign up as seller checkbox in sign up form

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -11,68 +11,98 @@ import {signup} from '@/api/auth.ts'
 import {useLogin} from '@/hooks/useLogin.ts'
 import ErrorMessage from '@/components/elements/ErrorMessage.tsx'
 import {getErrorMessage} from '@/lib/utils/error.ts'
-import {Checkbox} from '@/components/ui/checkbox.tsx'
-import {useState} from 'react'
 
 const SignupForm = ({}) => {
     const inputStyle = 'p-0 rounded-b-none border-x-0 border-t-0 border-black/50'
     const {t} = useTranslation(I18N_NAMESPACES.authPage)
-    const [isSeller, setIsSeller] = useState(false)
     const form = useForm<SignupFormType>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: '',
             email: '',
             password: '',
-            role: 'user',
+            role: 'user'
         }
     })
 
     const {mutate, error, isError} = useLogin(signup)
 
     const onSubmit = (values: SignupFormType) => {
-        mutate({ ...values, role: isSeller ? 'seller' : 'user' })
+        mutate({...values, role: 'user'})
     }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-[370px] flex-col gap-10 space-y-8">
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="max-w-[370px] flex-col gap-10 space-y-8">
                 <div className={'flex-col-start gap-6'}>
-                    <h2 className={'text-big-medium'}>{t(AUTH_LOCALES.headlineSignup)}</h2>
-                    <p className={'text-body'}>{t(AUTH_LOCALES.subheadingSignup)}</p>
+                    <h2 className={'text-big-medium'}>
+                        {t(AUTH_LOCALES.headlineSignup)}
+                    </h2>
+                    <p className={'text-body'}>
+                        {t(AUTH_LOCALES.subheadingSignup)}
+                    </p>
                 </div>
-                <FormInput name={'name'} placeholder={t(AUTH_LOCALES.name)} className={inputStyle}
-                           formControl={form.control}/>
-                <FormInput name={'email'} placeholder={t(AUTH_LOCALES.email)} type={'email'} className={inputStyle}
-                           formControl={form.control}/>
-                <FormInput name={'password'} placeholder={t(AUTH_LOCALES.password)} type={'password'}
-                           className={inputStyle}
-                           formControl={form.control}/>
-                <FormInput name={'passwordConfirm'} placeholder={t(AUTH_LOCALES.confirmPassword)} type={'password'}
-                           className={inputStyle}
-                           formControl={form.control}/>
-                <div className="flex items-center gap-2">
-                    <Checkbox id="seller-checkbox"
-                              checked={isSeller}
-                              onCheckedChange={(checked) => setIsSeller(checked as boolean)}
-                    />
-                    <label htmlFor="seller-checkbox"
-                           className="text-sm">
-                        {/*todo: add translation*/}
-                        Sign up as a seller
-                    </label>
-                </div>
+                <FormInput
+                    name={'name'}
+                    placeholder={t(AUTH_LOCALES.name)}
+                    className={inputStyle}
+                    formControl={form.control}
+                />
+                <FormInput
+                    name={'email'}
+                    placeholder={t(AUTH_LOCALES.email)}
+                    type={'email'}
+                    className={inputStyle}
+                    formControl={form.control}
+                />
+                <FormInput
+                    name={'password'}
+                    placeholder={t(AUTH_LOCALES.password)}
+                    type={'password'}
+                    className={inputStyle}
+                    formControl={form.control}
+                />
+                <FormInput
+                    name={'passwordConfirm'}
+                    placeholder={t(AUTH_LOCALES.confirmPassword)}
+                    type={'password'}
+                    className={inputStyle}
+                    formControl={form.control}/>
+                {/*<div className="flex items-center gap-2">*/}
+                {/*    <Checkbox id="admin-checkbox"*/}
+                {/*              checked={isAdmin}*/}
+                {/*              onCheckedChange={(checked) => setIsAdmin(checked as boolean)}*/}
+                {/*    />*/}
+                {/*    <label htmlFor="admin-checkbox"*/}
+                {/*           className="text-sm">*/}
+                {/*        /!*todo: add translation*!/*/}
+                {/*        Sign up as an Admin*/}
+                {/*    </label>*/}
+                {/*</div>*/}
 
                 <div className={'flex_col gap-4'}>
-                    <Button className={'w-full capitalize'} type="submit" text={t(AUTH_LOCALES.createAccount)}/>
-                    <Button className={'w-full'} variant={'white'} type={'button'}
-                            text={`${t(AUTH_LOCALES.signup)} ${t(AUTH_LOCALES.withGoogle)}`}/>
+                    <Button className={'w-full capitalize'}
+                            type="submit"
+                            text={t(AUTH_LOCALES.createAccount)}
+                    />
+                    <Button className={'w-full'}
+                            variant={'white'}
+                            type={'button'}
+                            text={`${t(AUTH_LOCALES.signup)} ${t(AUTH_LOCALES.withGoogle)}`}
+                    />
                 </div>
                 {isError && <ErrorMessage
                     message={getErrorMessage(error)}/>}
                 <div className={'flex-center-row gap-4'}>
-                    <p className={'text-body'}>{t(AUTH_LOCALES.alreadyHaveAccount)}</p>
-                    <Link to={'/login'} className={'text-body-medium underline'}>{t(AUTH_LOCALES.login)}</Link>
+                    <p className={'text-body'}>
+                        {t(AUTH_LOCALES.alreadyHaveAccount)}
+                    </p>
+                    <Link to={'/login'}
+                          className={'text-body-medium underline'}>
+                        {t(AUTH_LOCALES.login)}
+                    </Link>
                 </div>
             </form>
         </Form>
