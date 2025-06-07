17eaa8a686d8ae5fdef6755926088c65d0e34b58
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-06-08 01:15:21 +0300

    rfc: refactor checkout page

--- src/pages/store/CheckoutPage.tsx
+++ src/pages/store/CheckoutPage.tsx
@@ -1,3 +1,4 @@
+import * as defaultLanguage from '@/language'
 import {useEffect, useRef, useState} from 'react'
 import CheckoutForm from '@/components/checkout/CheckoutForm.tsx'
 import CheckoutSummary from '@/components/checkout/CheckoutSummary.tsx'
@@ -7,7 +8,6 @@ import {CHECKOUT_LOCALES, I18N_NAMESPACES} from '@/constants/locales.ts'
 import {Elements} from '@stripe/react-stripe-js'
 import {loadStripe, StripeElementLocale, StripeElementsOptions} from '@stripe/stripe-js'
 import {getPaymentSecret} from '@/api/payment.ts'
-import * as defaultLanguage from '@/language';
 import {stripeAppearance} from '@/constants/stripeElementConfig.ts'
 import {useSelector} from 'react-redux'
 import {IRootState} from '@/store'
@@ -19,9 +19,10 @@ const CheckoutPage = ({}) => {
     const {t} = useTranslation(I18N_NAMESPACES.checkout)
     const userInfoRef = useRef<FormRef | null>(null)
     const cart = useSelector((state: IRootState) => state.cart)
+
     // @ts-ignore
     const [language, setLanguage] = useState<StripeElementLocale>(defaultLanguage)
-    const [clientSecretKey, setClientSecretKey] = useState('')
+    const [clientSecretKey, setClientSecretKey] = useState<string>('')
 
     useEffect(() => {
         setLanguage(localStorage.getItem('language') as StripeElementLocale)
@@ -31,7 +32,8 @@ const CheckoutPage = ({}) => {
     useEffect(() => {
         const getSecret = async () => {
             try {
-                const secretKey = await getPaymentSecret(cart)
+                const secretKey = await getPaymentSecret(cart.total)
+
                 setClientSecretKey(secretKey)
             } catch (e: Error | any) {
                 console.error('Error fetching client secret:', e)
@@ -44,12 +46,14 @@ const CheckoutPage = ({}) => {
     const stripeOptions: StripeElementsOptions = {
         clientSecret: clientSecretKey,
         appearance: stripeAppearance,
-        locale: language as StripeElementLocale,
+        locale: language as StripeElementLocale
     }
 
     return (
         <main className={'flex_col ml-[5vw] w-[90vw] gap-12 max-sm:ml-[2.5vw] max-sm:w-[95vw]'}>
-            <h2 className={'text-big-medium'}>{t(CHECKOUT_LOCALES.checkout)}</h2>
+            <h2 className={'text-big-medium'}>
+                {t(CHECKOUT_LOCALES.checkout)}
+            </h2>
             {clientSecretKey &&
                 <Elements stripe={stripePromise} options={stripeOptions}>
                     <div className={'md:flex-row-between mb-10 gap-20'}>
